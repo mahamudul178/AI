a8f3744ae612c6164d53a4b8adf8a329670183cc
Mahamudul Hasan <123629948+mahamudul178@users.noreply.github.com>
2023-10-30 08:17:22 +0600

    Update 03_Depth_first_search.py

--- ICE-3102_(Artificial)_code/03_DFS/03_Depth_first_search.py
+++ ICE-3102_(Artificial)_code/03_DFS/03_Depth_first_search.py
@@ -1,23 +1,24 @@
+def dfs(graph, node, visited):
+  if node not in visited:
+    visited.add(node)
+    print("'" + node + "', " , end = '') # Print the current node
+    # Recursively explore all of the adjacent nodes.
+    for neighbor in graph[node]:
+      if neighbor not in visited:
+        dfs(graph, neighbor, visited)
 
-def DFS(graph, start):
-    visited=set()
-    stack=[start]
-    while stack:
-        node=stack.pop()
-        if node not in visited:
-            visited.add(node)
-            stack.extend(graph[node]- visited)
-    return visited
-    
-graph={
-    'A':set(['B','C',]),
-    'B':set(['A','D','E']),
-    'C':set(['A','F']),
-    'D':set(['B']),
-    'E':set(['B','F']),
-    'F':set(['C', 'E'])
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
 }
-
-print("DFS Traversal: ")
-dfs=DFS(graph, 'A')
-print(dfs)
+visited = set()
+print("DFS Traversal:")
+dfs(graph, 'A', visited)
+# If the graph is disconnected
+for node in graph:
+    if node not in visited:  
+      dfs(graph, node, visited)
